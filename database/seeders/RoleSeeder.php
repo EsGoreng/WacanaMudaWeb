c251b3728f3a5faed1fda08152ec75bffead2543
EsGoreng <itsnaakhdan25@gmail.com>
2026-01-11 09:58:30 +0700

    add Footer

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,28 +12,18 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1. Reset Cached Roles/Permissions (Wajib untuk Spatie)
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // 2. Buat Permissions (Berdasarkan Kebutuhan Fungsional)
-        // KF-CONT-01: Menulis konten
         Permission::create(['name' => 'create posts']);
         Permission::create(['name' => 'edit own posts']);
         Permission::create(['name' => 'delete own posts']);
         
-        // KF-INT-01: Komentar
         Permission::create(['name' => 'post comments']);
-        
-        // KF-USR-07: Validasi anggota baru
         Permission::create(['name' => 'validate members']);
         
-        // KF-EVT-01: Mengelola Event
         Permission::create(['name' => 'manage events']);
-        
-        // Moderasi Konten (Hapus/Edit konten orang lain jika melanggar)
         Permission::create(['name' => 'moderate content']);
         
-        // KF-USR-08: Mengelola akun pengurus
         Permission::create(['name' => 'manage admins']);
 
         $roleAnggota = Role::create(['name' => 'member']);
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="p-4 md:p-8 lg:p-10 bg-white antialiased dark:bg-linear-to-b dark:from-slate-700 dark:to-slate-800">
+    <div class="mx-auto max-w-screen-xl text-center">
+        <div class="flex justify-center mb-4 scale-125">
+            <x-app-logo class="text-white dark:text-black" />
+        </div>
+        <p class="my-6 text-gray-500 dark:text-gray-400">Ruang Kata - Jejak Karya - Jelajah Rasa</p>
+        <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Premium</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Campaigns</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Blog</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Affiliate Program</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">FAQs</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Contact</a>
+            </li>
+        </ul>
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#"
+                class="hover:underline">WacanMudaBerkarya™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
