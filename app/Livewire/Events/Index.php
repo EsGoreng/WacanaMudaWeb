c8892009be3807561fc90752b84d0d8c0c0b61e8
EsGoreng <itsnaakhdan25@gmail.com>
2026-01-27 14:33:26 +0700

    filter event

--- app/Livewire/Events/Index.php
+++ app/Livewire/Events/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Events;
 
+use App\Models\Category;
 use App\Models\Event;
 use App\Services\BookmarkService;
 use App\Services\StoryGeneratorService;
 use App\Traits\InteractsWithEventModal;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -18,6 +20,21 @@ class Index extends Component
     #[Url(as: 'id')]
     public $urlEventId = '';
 
+    #[Url(except: [])]
+    public $selectedCategories = [];
+
+    #[Url(except: '')]
+    public $search = '';
+
+    #[Url(except: 'latest')]
+    public $sortBy = 'latest';
+
+    #[Url(except: null)]
+    public $dateFrom = null;
+
+    #[Url(except: null)]
+    public $dateTo = null;
+
     public function mount(BookmarkService $bookmarkService)
     {
         if ($this->urlEventId) {
@@ -25,17 +42,67 @@ class Index extends Component
         }
     }
 
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSelectedCategories()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSortBy()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingDateFrom()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingDateTo()
+    {
+        $this->resetPage();
+    }
+
+    public function clearFilters()
+    {
+        $this->reset(['search', 'selectedCategories', 'sortBy', 'dateFrom', 'dateTo']);
+        $this->resetPage();
+    }
+
+    #[Computed]
+    public function categories()
+    {
+        $validStatuses = ['published', 'ongoing', 'ended', 'canceled'];
+
+        return Category::whereHas('events', function ($query) use ($validStatuses) {
+            $query->whereIn('status', $validStatuses);
+        })
+            ->withCount(['events' => function ($query) use ($validStatuses) {
+                $query->whereIn('status', $validStatuses);
+                if (! empty($this->search)) {
+                    $query->where(function ($q) {
+                        $q->where('title', 'like', '%'.$this->search.'%')
+                            ->orWhere('description', 'like', '%'.$this->search.'%')
+                            ->orWhere('location_name', 'like', '%'.$this->search.'%');
+                    });
+                }
+            }])
+            ->orderBy('name')
+            ->get();
+    }
+
     public function toggleEventBookmark($eventId, BookmarkService $service)
     {
         if (! auth()->check()) {
             return redirect()->route('login');
         }
-
         $event = Event::find($eventId);
-
         if ($event) {
             $service->toggleBookmark(auth()->user(), $event);
-
             if ($this->selectedEvent && $this->selectedEvent->id == $eventId) {
                 $this->isBookmarked = ! $this->isBookmarked;
             }
@@ -51,15 +118,56 @@ class Index extends Component
 
     public function render()
     {
-        $events = Event::query()
-            ->with('categories', 'bookmarks')
-            ->whereIn('status', ['published', 'ongoing', 'ended', 'canceled'])
-            ->orderByRaw("FIELD(status, 'published', 'ongoing', 'ended', 'canceled') ASC")
-            ->latest()
-            ->paginate(perPage: 6);
+        $query = Event::query()
+            ->with('categories', 'bookmarks');
+
+        $query->whereIn('status', ['published', 'ongoing', 'ended', 'canceled']);
+
+        if (! empty($this->search)) {
+            $query->where(function ($q) {
+                $q->where('title', 'like', '%'.$this->search.'%')
+                    ->orWhere('description', 'like', '%'.$this->search.'%')
+                    ->orWhere('location_name', 'like', '%'.$this->search.'%');
+            });
+        }
+
+        if (! empty($this->selectedCategories)) {
+            $query->whereHas('categories', function ($q) {
+                $q->whereIn('categories.category_id', $this->selectedCategories);
+            });
+        }
+
+        if (! empty($this->dateFrom)) {
+            $query->whereDate('start_time', '>=', $this->dateFrom);
+        }
+        if (! empty($this->dateTo)) {
+            $query->whereDate('start_time', '<=', $this->dateTo);
+        }
+
+        switch ($this->sortBy) {
+
+            case 'event_date_nearest':
+                $query->orderBy('start_time', 'asc');
+                break;
+            case 'event_date_furthest':
+                $query->orderBy('start_time', 'desc');
+                break;
+
+            case 'published_newest':
+                $query->latest();
+                break;
+            case 'published_oldest':
+                $query->oldest();
+                break;
+
+            default:
+                $query->orderByRaw("FIELD(status, 'published', 'ongoing', 'ended', 'canceled') ASC")
+                    ->orderBy('start_time', 'asc');
+                break;
+        }
 
         return view('livewire.events.index', [
-            'events' => $events,
+            'events' => $query->paginate(6),
         ]);
     }
 }
